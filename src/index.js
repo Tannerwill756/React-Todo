f0d5ab0362a61316010139632a981915c1a552c3
Tanner Williams <tannerwill756@gmail.com>
2020-04-07 11:48:37 -0400

    css styling

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div className="parent"><App /></div>, document.getElementById('root'));
